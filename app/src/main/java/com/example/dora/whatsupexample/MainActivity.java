e06aa5487142bbc2fe94c1558bbca8a38d310da9
tunnadora <tunna@edcast.com>
2017-07-12 10:18:06 +0530

    change in login page

--- app/src/main/java/com/example/dora/whatsupexample/MainActivity.java
+++ app/src/main/java/com/example/dora/whatsupexample/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.dora.whatsupexample;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,87 +18,130 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.id;
 
-    EditText uname , passwd;
-    Button signup;
-    private FirebaseAuth mauth;
-    private DatabaseReference mDatabase;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String tag = MainActivity.class.getSimpleName();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        uname=(EditText) findViewById(R.id.uname);
-        passwd=(EditText) findViewById(R.id.paaswd);
-        signup = (Button)findViewById(R.id.signup);
-        mauth=FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+        findViewById(R.id.signup).setOnClickListener(this);
+        initViews();
+    }
 
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    private Button mNextButton;
+    private EditText mUsernameET;
 
-                signUp();
+    private void initViews() {
+        mNextButton = (Button) findViewById(R.id.signup);
+        mNextButton.setOnClickListener(this);
+        mUsernameET = (EditText) findViewById(R.id.uname);
+    }
 
-            }
-        });
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signup:
+                onNextButtonClicked();
+
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
 
     }
 
-    private void signUp() {
-       final String email = uname.getText().toString();
-        final String password = passwd.getText().toString();
+    String mUserName;
 
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this , "PLEASE ENTER EMAIL ....",Toast.LENGTH_LONG).show();
-            return;
+    private void onNextButtonClicked() {
+
+        mUserName = mUsernameET.getText().toString();
+        if (!TextUtils.isEmpty(mUserName)) {
+            createUser();
+        } else {
+            Toast.makeText(this, "User name can not be blank", Toast.LENGTH_LONG).show();
         }
+    }
 
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this , "PLEASE ENTER A VALID PASSWORD ....",Toast.LENGTH_LONG).show();
-            return;
-        }
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
-        mauth.createUserWithEmailAndPassword(email, password)
+    private void createUser() {
+        mAuth = FirebaseAuth.getInstance();
+        String email = mUserName + "@gmail.com";
+
+        mAuth.createUserWithEmailAndPassword(email, mUserName)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(tag, "createUser:oncomplete:" + task.isSuccessful());
+
+                        hideProgressDialog();
+
+
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            String user_id = mauth.getCurrentUser().getUid();
-
-
-
-                            DatabaseReference current_u_id = mDatabase.child(user_id);
-
-
-                            current_u_id.child(email);
-
-
-                            uname.setText("");
-                            passwd.setText("");
-                            Toast.makeText(MainActivity.this, "Sign up successful..",Toast.LENGTH_LONG ).show();
-
-
-
-
+                            onAuthSuccess(task.getResult().getUser());
                         } else {
-                            // If sign in fails, display a message to the user.
-
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
+                            try {
+                                Exception e = task.getException();
+                                e.printStackTrace();
+                                Log.d("exception:", e.getMessage());
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                Log.d("exception:", e.getMessage());
+                            }
                         }
-
                     }
                 });
 
+    }
+
+
+    private void onAuthSuccess(FirebaseUser firebaseUser) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        User user = new User();
+        user.setName(usernameFromEmail(firebaseUser.getEmail()));
+        user.setUid(firebaseUser.getUid());
+        mDatabase.child("users").child(firebaseUser.getUid()).setValue(user);
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setMessage("Loading...");
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
     }
 
 }
